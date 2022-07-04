78979032ac095ed2dd5f208879081ce7636dc542
GreedNeSS <ruslan199126@gmail.com>
2022-07-04 20:32:56 +0300

    Added introduction example.

---
+++ Introduction/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Introduction.Models;
+
+namespace Introduction.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<Person> people = new List<Person>()
+        {
+            new Person(1, "GreedNeSS", 30),
+            new Person(2, "Marcus", 45),
+            new Person(3, "Henry", 24),
+        };
+
+        public IActionResult Index()
+        {
+            return View(people);
+        }
+    }
+}
---
+++ Introduction/Models/Person.cs
@@ -0,0 +1,4 @@
+﻿namespace Introduction.Models
+{
+    public record class Person(int Id, string Name, int Age);
+}
